8b4dfad7b6e7d8c178ef909eb725ed0e99c67739
simon <simonacostfc@gmail.com>
2025-12-05 11:02:08 -0500

    jugador en el main

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
-from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, UploadFile, File, Form, Request, HTTPException
+from fastapi.staticfiles import StaticFiles
+import jugador
+from db import create_tables
 
-app = FastAPI(title="sigmotoa FC")
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await create_tables(app)
+    yield
+
+
+app = FastAPI(lifespan=lifespan, title="sigmotoa FC")
+
+app.mount("/templates", StaticFiles(directory="templates"), name="templates")
+app.include_router(jugador.router, tags=["jugador"], prefix="/jugador")
 
 
 @app.get("/")
